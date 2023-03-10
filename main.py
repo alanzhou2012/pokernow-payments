fa588b7199e6d136d006b29efaed523ce98ecb47
Alan Zhou <alanzhou@alans-air.lan>
2023-03-10 12:14:53 -0800

    first pass

---
+++ main.py
@@ -0,0 +1,52 @@
+import argparse
+import csv
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str)
+    return parser.parse_args()
+
+def parse_csv(file_path):
+    net_dict = {}
+    with open(file_path, 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+
+        for row in reader:
+            add_net(row, net_dict)
+
+    print_payments(net_dict)
+
+def add_net(row, net_dict):
+    name = row['player_nickname']
+    row_net = int(row['net'])
+    net = row_net if name not in net_dict else net_dict[name] + row_net
+    net_dict[name] = net
+
+def print_payments(net_dict):
+    sorted_dict = sorted(net_dict.items(), key=lambda x:x[1], reverse=True)
+    sorted_dict = [list(ele) for ele in sorted_dict]
+    # print(sorted_dict)
+
+    winner_index = 0
+    loser_index = len(sorted_dict) - 1
+
+    while winner_index < loser_index:
+        winner_remaining = sorted_dict[winner_index][1]
+        loser_remaining = sorted_dict[loser_index][1]
+        winner_to_request = min(winner_remaining, abs(loser_remaining))
+
+        # print amount
+        print(sorted_dict[winner_index][0] + ' Request ' + str(winner_to_request/100) + ' From ' + sorted_dict[loser_index][0])
+
+        sorted_dict[winner_index][1] = winner_remaining - winner_to_request
+        sorted_dict[loser_index][1] += winner_to_request
+
+        if sorted_dict[winner_index][1] == 0:
+            winner_index += 1
+
+        if sorted_dict[loser_index][1] == 0:
+            loser_index -= 1
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    args = parse_args()
+    parse_csv(args.path)
